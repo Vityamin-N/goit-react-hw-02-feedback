4832159f0c36a2d07371466b77c32a1968d7bf5e
Vityamin-N <heartless41196@gmail.com>
2022-05-09 04:30:28 +0300

    complete task

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import { Component, Fragment } from "react";
+import Section from "components/Section";
+import Controls from "components/Controls";
+import Statistics from "components/Statistics";
+import Notification from "components/Notification";
+
+class App extends Component {
+  state = {
+    Good: 0,
+    Neutral: 0,
+    Bad: 0,
+  };
+
+  changeState = (e) =>
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        [e.target.dataset.value]: prevState[e.target.dataset.value] + 1,
+      };
+    });
+
+  makeExtendedStats = (stats) => {
+    const total = this.countTotalFeedback(stats);
+    const percentGood = this.countPositiveFeedbackPercentage(stats, total);
+    return [
+      ...stats,
+      { label: "Total", count: total },
+      { label: "Positive feedback", count: percentGood },
+    ];
+  };
+
+  countTotalFeedback = (stats) => {
+    return stats.reduce((total, item) => (total += item.count), 0);
+  };
+
+  countPositiveFeedbackPercentage = (stats, total) => {
+    const totalGood = stats.reduce(
+      (total, item) => (item.label === "Good" ? (total += item.count) : total),
+      0
+    );
+    return total > 0 ? String(Math.round((totalGood / total) * 100)) + "%" : "";
+  };
+
+  render() {
+    const stats = Object.entries(this.state).map(([label, count]) => ({
+      label,
+      count,
+    }));
+    const extendedStats = this.makeExtendedStats(stats);
+    return (
+      <Fragment>
+        <h1>Please leave feedback</h1>
+        <Section>
+          <Controls
+            options={Object.keys(this.state)}
+            onClick={this.changeState}
+          />
+        </Section>
+        {this.countTotalFeedback(stats) ? (
+          <Section type="statistics" title="Statistics">
+            <Statistics stats={extendedStats} />
+          </Section>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Fragment>
+    );
+  }
+}
+export default App;
